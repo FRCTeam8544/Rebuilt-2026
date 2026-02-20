6afe42a36842d6f6bd7da10af4d3da3cf5e9603b
TomServo-0x2A <tomservo@galacticfringe.com>
2026-02-19 23:02:44 -0500

    Add new FF command for shooter

--- 8544Rebuilt/src/main/java/frc/robot/commands/ShooterCommands.java
+++ 8544Rebuilt/src/main/java/frc/robot/commands/ShooterCommands.java
@@ -1,7 +1,12 @@
 package frc.robot.commands;
 
+import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.shooter.Shooter;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
@@ -10,6 +15,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -17,6 +23,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class ShooterCommands {
 
     private static final double DEADBAND = 0.1;
+    private static final double FF_START_DELAY = 2.0; // Secs
+    private static final double FF_RAMP_RATE = 0.1; // Volts/Sec
 
     private ShooterCommands() {}
 
@@ -184,5 +192,73 @@ public class ShooterCommands {
         },
         shooter);
     }
+
+    
+  /**
+   * Measures the velocity feedforward constants for the shooter motors.
+   *
+   * <p>This command should only be used in voltage control mode.
+   */
+  public static Command feedforwardCharacterization(Shooter shooter) {
+    List<Double> velocitySamples = new LinkedList<>();
+    List<Double> voltageSamples = new LinkedList<>();
+    
+    Timer timer = new Timer();
+
+    return Commands.sequence(
+        // Reset data
+        Commands.runOnce(
+            () -> {
+              velocitySamples.clear();
+              voltageSamples.clear();
+            }),
+
+        // Warmup
+        Commands.run(
+                () -> {
+                  shooter.runShooterOpenLoop(0.0);
+                },
+                shooter)
+            .withTimeout(FF_START_DELAY),
+
+        // Start timer
+        Commands.runOnce(timer::restart),
+
+        // Accelerate and gather data
+        Commands.run(
+                () -> {
+                  if (shooter.getFlywheelVelocityRPM() < Shooter.Flywheel.kMaxShooterRPM) {
+                    double voltage = timer.get() * FF_RAMP_RATE;
+                    shooter.runShooterOpenLoop(voltage / 12.0);
+                    velocitySamples.add(shooter.getShooterFFCharacterizationVelocity());
+                    voltageSamples.add(voltage);
+                  }
+                },
+                shooter)
+
+            // When cancelled, calculate and print results
+            .finallyDo(
+                () -> {
+                  int n = velocitySamples.size();
+                  double sumX = 0.0;
+                  double sumY = 0.0;
+                  double sumXY = 0.0;
+                  double sumX2 = 0.0;
+                  for (int i = 0; i < n; i++) {
+                    sumX += velocitySamples.get(i);
+                    sumY += voltageSamples.get(i);
+                    sumXY += velocitySamples.get(i) * voltageSamples.get(i);
+                    sumX2 += velocitySamples.get(i) * velocitySamples.get(i);
+                  }
+                  double kS = (sumY * sumX2 - sumX * sumXY) / (n * sumX2 - sumX * sumX);
+                  double kV = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+
+                  NumberFormat formatter = new DecimalFormat("#0.00000");
+                  System.out.println("********** Shooter FF Characterization Results **********");
+                  System.out.println("\tkS: " + formatter.format(kS));
+                  System.out.println("\tkV: " + formatter.format(kV));
+                }));
+  }
+
 
 }
