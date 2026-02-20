4f7079ff036edd37602ef0e41b80ccab5bebde80
TomServo-0x2A <tomservo@galacticfringe.com>
2026-02-20 16:47:47 -0500

    Sanity check

--- 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
+++ 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,14 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "8544Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 32;
-  public static final String GIT_SHA = "3aaa0dbb49210b2ad2b56a6aada0b31eb5056d5d";
-  public static final String GIT_DATE = "2026-02-20 13:49:55 EST";
-  public static final String GIT_BRANCH = "krakenShooter";
   public static final String BUILD_DATE = "2026-02-20 14:47:14 EST";
-  public static final long BUILD_UNIX_TIME = 1771616834967L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 52;
+  public static final String GIT_SHA = "d84122496ea1f1518f0f4fe46c50fbe8f67af514";
+  public static final String GIT_DATE = "2026-02-20 16:44:09 EST";
+  public static final String GIT_BRANCH = "Intake";
+  public static final String BUILD_DATE = "2026-02-20 16:44:49 EST";
+  public static final long BUILD_UNIX_TIME = 1771623889605L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
