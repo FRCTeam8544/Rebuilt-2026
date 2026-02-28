505039801ca011297f6230075a35e0044c596749
TomServo-0x2A <tomservo@galacticfringe.com>
2026-02-28 17:11:25 -0500

    Boost kV to hit 2700

--- 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
+++ 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "8544Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 81;
-  public static final String GIT_SHA = "6fc878fb474054b99af3bab27178b67c84bc248a";
-  public static final String GIT_DATE = "2026-02-28 16:37:04 EST";
+  public static final int GIT_REVISION = 82;
+  public static final String GIT_SHA = "a43f69ad6bd52a55eb811c953fe6ed6deb3383a9";
+  public static final String GIT_DATE = "2026-02-28 17:00:46 EST";
   public static final String GIT_BRANCH = "ReleaseTheKraken";
-  public static final String BUILD_DATE = "2026-02-28 16:58:37 EST";
-  public static final long BUILD_UNIX_TIME = 1772315917957L;
+  public static final String BUILD_DATE = "2026-02-28 17:10:04 EST";
+  public static final long BUILD_UNIX_TIME = 1772316604261L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- 8544Rebuilt/src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
+++ 8544Rebuilt/src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
@@ -30,8 +30,9 @@ public class ShooterIOTalonFX implements ShooterIO {
     private static final double kMeasuredKv = 680.0 / 60.0; // at 2700ish then convert rps
     //private static final double kV = (1.0 / kMeasuredKv) * 5; // Convert to V per RPS
     //private static final double kV = (1.0 / kMeasuredKv) * 5; // Convert to V per RPS
-    private static final double kV = 0.155; // For 32.1 rps
-
+  //  private static final double kV = 0.89; // For 32.1 rps 155,   .758
+    private static final double kV = 0.89; // For 32.1 rps 155,   .758
+    
     //private static final double kS = 1.8; // AMps // 0.33 was
     private static final double kS = 1.74;
     //private static final double kP = 10.0 / 32.1; // 0.2778; 36 is target rps of motor
