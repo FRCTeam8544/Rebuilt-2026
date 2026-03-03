d01f38ef298bd0b5addaffe262df89f0a96a43fa
TomServo-0x2A <tomservo@galacticfringe.com>
2026-03-02 21:39:01 -0500

    Incorporate refactor work from mecha_integration
    The shooter tuning is still needed. Weird binding at 2200 RPM.
    
    This is a merge up from main

--- 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
+++ 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "8544Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 87;
-  public static final String GIT_SHA = "1cf2fa6cfc968f408fdf4dbe029d786c607655f6";
-  public static final String GIT_DATE = "2026-03-02 11:16:40 EST";
+  public static final int GIT_REVISION = 97;
+  public static final String GIT_SHA = "be98970c1f7bbc934a25508e08a03c61a270345c";
+  public static final String GIT_DATE = "2026-03-02 21:37:06 EST";
   public static final String GIT_BRANCH = "ReleaseTheKraken";
-  public static final String BUILD_DATE = "2026-03-02 20:57:45 EST";
-  public static final long BUILD_UNIX_TIME = 1772503065172L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-02 21:37:43 EST";
+  public static final long BUILD_UNIX_TIME = 1772505463499L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
