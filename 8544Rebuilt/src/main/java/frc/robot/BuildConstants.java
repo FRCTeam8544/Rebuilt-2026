77a6121ba248faaa989f5185b801a4c2c614501c
TomServo-0x2A <tomservo@galacticfringe.com>
2026-03-07 10:53:19 -0500

    Add basic vision instance
    Rough calibration of the camera position for driver camera and basic pose estimation.

--- 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
+++ 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "8544Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 108;
-  public static final String GIT_SHA = "f789db3558b26e8c4ee51ca33a23afbc9d6b63b3";
-  public static final String GIT_DATE = "2026-03-06 16:58:15 EST";
+  public static final int GIT_REVISION = 121;
+  public static final String GIT_SHA = "c9ac6ae5784321a003d431b035d09cdbe2c90f9b";
+  public static final String GIT_DATE = "2026-03-07 10:51:15 EST";
   public static final String GIT_BRANCH = "Vision";
-  public static final String BUILD_DATE = "2026-03-06 19:05:45 EST";
-  public static final long BUILD_UNIX_TIME = 1772841945860L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-07 10:51:27 EST";
+  public static final long BUILD_UNIX_TIME = 1772898687839L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
