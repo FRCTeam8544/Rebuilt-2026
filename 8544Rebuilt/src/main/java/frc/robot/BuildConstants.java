245985e65cbe776740960706c2fe6860839857dd
AM2024426 <alex.mccabe2023@gmail.com>
2026-03-20 09:58:40 -0400

    Use use front april tag camera instead

--- 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
+++ 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "8544Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 180;
-  public static final String GIT_SHA = "2ce84fe12522d5ae5815228e7f93f908ef7f183d";
-  public static final String GIT_DATE = "2026-03-19 21:36:07 EDT";
+  public static final int GIT_REVISION = 185;
+  public static final String GIT_SHA = "10998dafdde7f1172c3169d357b4d433e4d2671e";
+  public static final String GIT_DATE = "2026-03-20 09:09:07 EDT";
   public static final String GIT_BRANCH = "URI";
-  public static final String BUILD_DATE = "2026-03-19 21:37:19 EDT";
-  public static final long BUILD_UNIX_TIME = 1773970639470L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-03-20 09:57:53 EDT";
+  public static final long BUILD_UNIX_TIME = 1774015073295L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
