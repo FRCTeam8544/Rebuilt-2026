41c1e896c6a8f7fb0910c4c5ba50228e47e4682b
AM2024426 <alex.mccabe2023@gmail.com>
2026-03-19 21:42:50 -0400

    Update BuildConstants.java

--- 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
+++ 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "8544Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 179;
-  public static final String GIT_SHA = "d92bc2bb4d9ec983eb96d3be834c6d3b369434d3";
-  public static final String GIT_DATE = "2026-03-19 17:29:24 EDT";
+  public static final int GIT_REVISION = 180;
+  public static final String GIT_SHA = "2ce84fe12522d5ae5815228e7f93f908ef7f183d";
+  public static final String GIT_DATE = "2026-03-19 21:36:07 EDT";
   public static final String GIT_BRANCH = "URI";
-  public static final String BUILD_DATE = "2026-03-19 20:35:48 EDT";
-  public static final long BUILD_UNIX_TIME = 1773966948424L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-19 21:37:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1773970639470L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
