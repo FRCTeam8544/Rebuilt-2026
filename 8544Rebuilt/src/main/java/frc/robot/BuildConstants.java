2ce84fe12522d5ae5815228e7f93f908ef7f183d
AM2024426 <alex.mccabe2023@gmail.com>
2026-03-19 21:36:07 -0400

    Updated arm limits and TunerConstants

--- 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
+++ 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "8544Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 171;
-  public static final String GIT_SHA = "857e00def67a39b8fcd6a26a7c234f384254a450";
-  public static final String GIT_DATE = "2026-03-15 18:12:54 EDT";
-  public static final String GIT_BRANCH = "pathplanner";
-  public static final String BUILD_DATE = "2026-03-16 17:29:42 EDT";
-  public static final long BUILD_UNIX_TIME = 1773696582768L;
+  public static final int GIT_REVISION = 179;
+  public static final String GIT_SHA = "d92bc2bb4d9ec983eb96d3be834c6d3b369434d3";
+  public static final String GIT_DATE = "2026-03-19 17:29:24 EDT";
+  public static final String GIT_BRANCH = "URI";
+  public static final String BUILD_DATE = "2026-03-19 20:35:48 EDT";
+  public static final long BUILD_UNIX_TIME = 1773966948424L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- 8544Rebuilt/src/main/java/frc/robot/subsystems/Arm/ArmIO.java
+++ 8544Rebuilt/src/main/java/frc/robot/subsystems/Arm/ArmIO.java
@@ -6,10 +6,10 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ArmIO {
   
   // Encoder arm limits in rotations
-  public static final double kArmForwardLimit = 0.78;
-  public static final double kArmReverseLimit = 0.03;
+  public static final double kArmForwardLimit = 0.74;
+  public static final double kArmReverseLimit = 0.016;
   public static final double kNominalDeployPosition = kArmForwardLimit;
-  public static final double kNominalStowPosition = 0.037;
+  public static final double kNominalStowPosition = 0.021;
 
   public static final double kMotorToOutputRatio = 3.0;
   public static final double kOutputToMotorRatio = 1.0 / kMotorToOutputRatio;
