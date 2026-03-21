35a5a2a259fc5857bf01bc3e9f328221de2b7952
chrisnre <chris@reid-eriksen.com>
2026-03-21 14:00:41 -0400

    Fix hopper deploy/retract not stopping motor on command end
    Switch from Commands.run() to Commands.runEnd() so the arm motor stops
    when the command is interrupted by PathPlanner deadline or timeout.
    Remove encoder-based .until() termination since the absolute encoder is
    unreliable with the slipping belt.

--- 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
+++ 8544Rebuilt/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "8544Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 193;
-  public static final String GIT_SHA = "e96ff50f5cfd61c2937ab1cac1e187f7722fc6d5";
-  public static final String GIT_DATE = "2026-03-20 20:57:56 EDT";
+  public static final int GIT_REVISION = 194;
+  public static final String GIT_SHA = "b16038608082e51319000b6e31fa39e0b15416e5";
+  public static final String GIT_DATE = "2026-03-21 13:38:18 EDT";
   public static final String GIT_BRANCH = "URI";
-  public static final String BUILD_DATE = "2026-03-21 12:24:25 EDT";
-  public static final long BUILD_UNIX_TIME = 1774110265004L;
+  public static final String BUILD_DATE = "2026-03-21 13:59:39 EDT";
+  public static final long BUILD_UNIX_TIME = 1774115979480L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
